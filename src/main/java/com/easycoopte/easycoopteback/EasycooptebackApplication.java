ebcf65bfb8759ef8571b09c48b706917fad8568b
Mathieu Domichard <mathieu.domichard@gmail.com>
2023-11-22 13:01:30 +0100

    Initial commit

---
+++ src/main/java/com/easycoopte/easycoopteback/EasycooptebackApplication.java
@@ -0,0 +1,13 @@
+package com.easycoopte.easycoopteback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasycooptebackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasycooptebackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/easycoopte/easycoopteback/EasycooptebackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.easycoopte.easycoopteback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasycooptebackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
